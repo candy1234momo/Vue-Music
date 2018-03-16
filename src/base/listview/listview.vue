7f0bbb98a894984fbb9ebf92a846d0bdfd500f52
糖果 <837437211@qq.com>
2018-03-17 00:22:59 +0800

    lff

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,5 +1,5 @@
 <template>
-	<scroll :data="data" class="listview">
+	<scroll :data="data" class="listview" ref="listview">
 		<ul>
 			<li v-for="group in data" class="list-group" ref="listGroup">
 				<h2 class="list-group-title">{{group.title}}</h2>
@@ -11,8 +11,8 @@
 				</ul>
 			</li>
 		</ul>
-		<div>
-			<ul class="list-shortcut">
+		<div class="list-shortcut" @touchstart.stop.prevent="onShortcutTouchStart" @touchmove.stop.prevent="onshortcutTouchMove">
+			<ul>
 				<li v-for="(item,index) in shortcutList" class="item" :data-index="index">
 					{{item}}
 				</li>
@@ -22,6 +22,10 @@
 </template>
 <script type="text/javascript">
 import Scroll from 'base/scroll/scroll'
+import {getData} from 'common/js/dom'
+
+const ANCHOR_HEIGHT = 18
+
 	export default {
 		props:{
 			data:{
@@ -36,6 +40,39 @@ import Scroll from 'base/scroll/scroll'
 				})
 			}
 		},
+    data(){
+      return{
+        scrollY:-1,
+        currentIndex:0,
+        diff:-1
+      }
+    },
+    created(){
+      this.touch = {}
+    },
+    methods:{
+      onShortcutTouchStart(e){
+        let anchorIndex = getData(e.target, 'index')
+        console.log(anchorIndex);
+        let fristTouch = e.touches[0]
+        this.touch.y1 = fristTouch.pageY
+        this.touch.anchorIndex = anchorIndex
+
+        this._scrollTo(anchorIndex)
+      },
+      onshortcutTouchMove(e){
+        let fristTouch=e.touches[0]
+        this.touch.y2 = fristTouch.pageY
+        let delta = (this.touch.y2-this.touch.y1)/ANCHOR_HEIGHT | 0
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+        console.log(anchorIndex);
+        
+        this._scrollTo(anchorIndex)
+      },
+      _scrollTo(index){
+        this.$refs.listview.scrollToElement(this.$refs.listGroup[index])
+      }
+    },
 		components:{
 			Scroll
 		}
--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -14,6 +14,7 @@ export function addClass(el, className) {
 }
 
 export function getData(el , name , val){
+  console.log(el);
      const prefix = 'data-'
      name = prefix + name
      if(val){
